1263e40cf0f370fafa8232c1e64487e6d43f058a
Voicemaster89 <141681752+Voicemaster89@users.noreply.github.com>
2026-04-06 20:53:59 -0300

    Update persona.py

--- persona.py
+++ persona.py
@@ -1,1 +1,12 @@
+class Persona:
+    def __init__(self, nombre):
+        self._nombre = nombre
 
+    def get_nombre(self):
+        return self._nombre
+
+    def set_nombre(self, nombre):
+        self._nombre = nombre
+
+    def __str__(self):
+        return self._nombre
