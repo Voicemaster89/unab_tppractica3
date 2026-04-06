f7667e74200bafa9510db4389b06fce1994f5d05
Voicemaster89 <141681752+Voicemaster89@users.noreply.github.com>
2026-04-06 20:20:43 -0300

    Update punto.py

--- punto.py
+++ punto.py
@@ -1,1 +1,19 @@
+class punto:
+  def __int__:(self, x, y):
+    self.x= x
+    self.y= y
+    
+  def eje_X(self):
+    return self.x
 
+  def eje_y(self):
+    return self.y
+
+  def impresion(self):
+    return f"{(self.x}, {self.y})"
+
+  def opuesto(self):
+    return punto(- self.x, -self.y) 
+
+  def esta_en_eje_y(self):
+    return self.y == 0
