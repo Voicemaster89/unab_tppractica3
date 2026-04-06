7ce26e2ea970d89a9a54185433d96cc2a2be384b
Voicemaster89 <141681752+Voicemaster89@users.noreply.github.com>
2026-04-06 20:54:46 -0300

    Update libro.py

--- libro.py
+++ libro.py
@@ -1,1 +1,13 @@
+from persona import Persona
 
+class Libro:
+    def __init__(self, titulo, autor, isbn, paginas, edicion, editorial, ciudad, pais, fecha):
+        self._titulo = titulo
+        self._autor = autor  # objeto Persona
+        self._isbn = isbn
+        self._paginas = paginas
+        self._edicion = edicion
+        self._editorial = editorial
+        self._ciudad = ciudad
+        self._pais = pais
+        self._fecha = fecha 
