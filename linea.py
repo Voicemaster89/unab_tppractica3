2ecdeccf8b8fa64215ecca74251ecdb390fa1696
Voicemaster89 <141681752+Voicemaster89@users.noreply.github.com>
2026-04-06 20:40:27 -0300

    Update linea.py

--- linea.py
+++ linea.py
@@ -1,1 +1,22 @@
+from punto import punto 
 
+class linea:
+  def __init__(self , punto_a, punto_b):
+    self._punto_a =punto_a
+    self._punto_b =punto_b 
+
+  def mueve_derecha(self, distancia):
+    self._punta_a.x += distancia
+    self._punta_b.x += distancia
+
+  def mueve_izquierda(self, distancia):
+    self._punto_a.x -= distancia
+    self._puntoa_b.x -= distancia
+
+  def mover_hacia_arriba(self, distancia):
+    self._punto_a.y += distancia
+    self._punto_b.y += distancia
+
+  def mover_hacia_abajo(self, distancia):
+    self._punto_a.y -= distancia
+    self._punto_b.y -= distancia 
