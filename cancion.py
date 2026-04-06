2682a7eafa53f600c6213923901a10d6409b753f
Voicemaster89 <141681752+Voicemaster89@users.noreply.github.com>
2026-04-06 20:52:17 -0300

    Update cancion.py

--- cancion.py
+++ cancion.py
@@ -1,1 +1,19 @@
+class Cancion:
+    def __init__(self, titulo, autor):
+        self._titulo = titulo
+        self._autor = autor
 
+    def get_titulo(self):
+        return self._titulo
+
+    def get_autor(self):
+        return self._autor
+
+    def set_titulo(self, titulo):
+        self._titulo = titulo
+
+    def set_autor(self, autor):
+        self._autor = autor
+
+    def __str__(self):
+        return f"{self._titulo} - {self._autor}"
